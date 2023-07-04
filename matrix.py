af72c1d1aa027625d07a83951ae754f07ba07891
mikitka <mikitka266@gmail.com>
2023-07-04 19:09:43 +0300

    Initial commit

---
+++ matrix.py
@@ -0,0 +1,21 @@
+
+
+
+class Matrix:
+
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
+        A = [ [0]*a for i in range(b) ]
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                print(A[i][j], end = ' ')
+            print()
+
+
+    def sum(self):
+        print(a+b)
+        print()
+        print(np.add(a,b))
+        print(x + arr)
+
